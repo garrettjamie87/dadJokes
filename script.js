8d4bf0fc655722bc442c8d8165559478be1275b7
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-22 16:59:05 +0100

    create generateJoke function with fetch request

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke()
+const config = {
+      headers: {
+            'Accept': 'application/json'
+      }
+}
+
+
+function generateJoke(){
+      fetch('https://icanhazdadjoke.com', config)
+}
+
+
