ebcd5fb95f6a848900b379297ca62e2504c44aec
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-22 17:03:46 +0100

    refactor function for api call and now it works in console

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const jokeEl = document.getElementById('joke');
-const jokeBtn = document.getElementById('jokeBtn');
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
-generateJoke()
-const config = {
-      headers: {
-            'Accept': 'application/json'
-      }
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
-
-
-function generateJoke(){
-      fetch('https://icanhazdadjoke.com', config)
-}
-
-
